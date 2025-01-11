063d79988eb5ea20fd9f600b587527f7ea8c29f4
Hardik <hardikmahajan97@gmail.com>
2025-01-11 18:07:29 +0530

    Added the UI and functionality for Singup page for Vendor

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import {React, useEffect} from "react";
+
+
+export default function Login(){
+    const handleSubmit = aysnc (e) => {
+
+    };
+
+    return(
+        <>
+            <div>
+                <form onSubmit={handleSubmit}>
+
+                </form>
+            </div>
+        </>
+    )
+}
